30c53d5261804b3abd557967ca539c214a534bce
virtuezzysm202 <162703883+virtuezzysm202@users.noreply.github.com>
2025-04-27 12:47:19 +0700

    Membuat Health Category BE page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoute = require('./routes/user_route.js');
+const categoryRoute = require('./routes/category_route.js'); // Import category route
+const articleRoute = require('./routes/article_route.js'); // Import article route
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 
 app.use('/api/user', userRoute);
+app.use('/api/categories', categoryRoute); // Menggunakan route kategori
+app.use('/api/articles', articleRoute); // Route artikel
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
