597dadaa30c879299601809605f30137797679bb
virtuezzysm202 <162703883+virtuezzysm202@users.noreply.github.com>
2025-05-15 20:04:49 +0700

    update for fetch api

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoute = require('./routes/user_route.js');
+const cors = require('cors');
 const categoryRoute = require('./routes/category_route.js'); // Import category route
 const articleRoute = require('./routes/article_route.js'); // Import article route
 require('dotenv').config();
 var cors = require('cors')
 
 const app = express();
+
 const PORT = process.env.PORT || 8080;
+app.use(cors());
 
 app.use(cors())
 
