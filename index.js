b9fa70c5d38cb85be6322919b6a0c896f143d678
tius13 <ignatiusalvin263@gmail.com>
2025-06-15 00:45:18 +0700

    membuat yg di perlukan

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const userRoute = require('./routes/user_route.js');
-const Remind = require('./routes/remind.js');
-require('dotenv').config();
-
+const cors = require('cors');
 const app = express();
+const remindRoutes = require('./routes/remind');
+const userRoutes = require('./routes/user_route');
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
+require('dotenv').config();
 
-app.use('/api/user', userRoute);
-app.use('/api/reminder', Remind);
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', remindRoutes);     
+app.use('/api/user', userRoutes);  
 
 app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
+
+  console.log(`Server running at http://localhost:${PORT}`);
+
 });
+
+ 
