6fa94fdd22fc8617b5c1018e18917a54b84e2cda
dimashadi <dimashadianto460@gmail.com>
2025-06-14 09:02:39 +0700

    craete refresh token and auth user

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const userRoute = require('./routes/user_route.js');
 const categoryRoute = require('./routes/category_route.js'); 
 const articleRoute = require('./routes/article_route.js');
 const workoutRoute = require('./routes/workout_route.js'); 
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
@@ -12,7 +13,11 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use('/api/user', userRoute);
--- models/user_model.js
+++ models/user_model.js
@@ -12,8 +12,25 @@ const resetPassword = (user, callback) => {
     db.query('UPDATE users SET password = ? WHERE email = ?', [user.password, user.email], callback);
 };
 
+const saveOrUpdateRefreshToken = (userId, token, callback) => {
+    const sql = 'INSERT INTO refresh_tokens (user_id, token) VALUES (?, ?) ON DUPLICATE KEY UPDATE token = VALUES(token)';
+    db.query(sql, [userId, token], callback);
+};
+
+
+const findByRefreshToken = (refreshToken, callback) => {
+    db.query('SELECT * FROM refresh_tokens WHERE token = ?', [refreshToken], callback);
+};
+
+const deleteRefreshTokenByUserId = (userId, callback) => {
+    db.query('UPDATE refresh_tokens SET token = NULL WHERE user_id = ?', [userId], callback);
+};
+
 module.exports = {
     findUserByEmail,
     createUser,
-    resetPassword
+    resetPassword,
+    saveOrUpdateRefreshToken,
+    findByRefreshToken,
+    deleteRefreshTokenByUserId,
 };
